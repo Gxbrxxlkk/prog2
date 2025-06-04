2a73f28b28edd16cdb87de9ca899274d769799c8
Gabriel Pivetta Loss <gabriellossoficial@gmail.com>
2025-06-04 17:05:06 -0300

    Add files via upload

---
+++ aula_introdutoria.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int numero{'B'};
+    int n2{'C'};
+    int n3{'D'};
+    int n4{'b'};
+    int n5{'c'};
+    int num{0};
+
+    
+
+    cout << numero << ' ' << n2 <<' ' << n3 << ' ' << n4 << ' ' << n5;
+    //cout << "Print do C++\n";
+    //cout << "\tEscreva algo abaixo:" <<endl;
+
+    //cin >> numero >> letra >> numero1 >> flag;
+
+    //cout << numero << " " << letra << " " << numero1 << " " << boolalpha << flag; //boolalpha precisa de std:: se não tiver a segunda linha do código e serve par mostrar booleano e não 1 e 0
+
+    return 0;
+}
